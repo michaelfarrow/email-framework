b5f17f20678bda97e2dcf79c060f1454f7d6ea3e
Mike Farrow <contact@mikefarrow.co.uk>
2015-08-10 16:37:48 +0100

    adding a few build tools and spacer.gif

--- gulpfile.js
+++ gulpfile.js
@@ -3,19 +3,33 @@ var gulp      = require('gulp'),
     inline    = require('gulp-inline'),
     inlineCss = require('gulp-inline-css')
     zip       = require('gulp-zip'),
-    minify    = require('gulp-minify-css');
+    minify    = require('gulp-minify-css'),
+    htmllint  = require('gulp-htmllint'),
+    csslint   = require('gulp-csslint');
 
+
+gulp.task('htmllint', function () {
+	return gulp.src('src/email.html')
+		.pipe(htmllint('.htmllintrc'));
+});
+
+gulp.task('csslint', function () {
+	return gulp.src('src/style.css')
+		.pipe(csslint('.csslintrc'))
+		.pipe(csslint.reporter());
+});
 
 gulp.task('copy', function () {
 	return gulp.src('src/images/**/*')
 		.pipe(gulp.dest('build/images'));
 });
 
-gulp.task('compile', function () {
+gulp.task('compile', ['htmllint', 'csslint'], function () {
 	return gulp.src('src/email.html')
 		.pipe(inline({
 			base: 'src/',
-			css: minify()
+			css: minify(),
+			disabledTypes: ['svg', 'img', 'js']
 		}))
 		.pipe(inlineCss({
 			removeStyleTags: false
@@ -30,6 +44,11 @@ gulp.task('zip', ['copy', 'compile'], function () {
 		], { base_dir: 'build' })
 		.pipe(zip('build.zip'))
 		.pipe(gulp.dest('build'));
+});
+
+gulp.task('watch', function() {
+	gulp.watch('src/*.html', ['htmllint']);
+	gulp.watch('src/*.css', ['csslint']);
 });
 
 gulp.task('build', [
